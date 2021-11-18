a46dc10a1fc474f475e115ab5f1e1411a3eab883
Parth Sanghavi <parthsanghavi.017@gmail.com>
2021-11-18 19:41:16 +0530

    Assignment

---
+++ parth_assignment1.py
@@ -0,0 +1,46 @@
+#Question 1
+sequence = input("Enter a DNA sequence:")
+length =  len(sequence)
+print(length)
+
+#Question 2 
+GCcont = 100 *((sequence.count("G")+ sequence.count("C")) / length)
+print (GCcont, "percent is the GC cont")
+
+#Question 3
+mutated = sequence.replace("T" , "A")
+print(mutated)
+
+#Question 4 
+half = length//2
+A = sequence[half:]
+B = sequence [:half]
+
+print(A)
+print(B)
+
+#Question 5
+seq = ("ATCGATAGATACAA") #Since ATA start from in POS 4
+seq.find("ATA")
+
+
+#Question 6 
+seq_2 = {"UAAAAGGCGAGAUAAAUA"}
+seq_2.find("UAG")
+seq_2.find("UAA")
+seq_2.find("UGA")
+
+#Question 7 
+#delta = ()
+#delta.find('ACGTC')
+#delta.find('CTAGT')
+#delta.find('ATGCTA')
+
+#Question 8 
+input_seq = "ATGATAGATAGATGATATCGATAGTA"
+len_input = len(input_seq)
+input_half = len_input//2
+second = input_seq[input_half:]
+reversed_second = second[::-1]
+print(reversed_second)
+
---
+++ parth_assignment3.py
@@ -0,0 +1,18 @@
+bcell_iedb={"ANPYFSQRMTPYVPKQKKVTKKLRTTTSKPSNKKPDSVRAIDSNATN","S","PSIPI","NEKVTKGAP","KVTKAAGAP","KVTKGAP","LSISDYFIPLEKSTLVPKK","GAPNLET","WMDPQKLAALRELVPEIPKIWADL","LSISDYFIPLEKSTLVPKK","LRTTTSKPSN" }
+bcell_bepipred={"ANPYFSQRMTPYVPKQKKVTKKKKPDSVRAIDSNATN","S","PSIPI","NEKVTKGAP","KVTKGAP","LSISDYFIPLEKSTLVPKK","GAPNLET","WMDPQKLAALRELVPEIPKIWADL","LSISDYFIPLEKSTLVPKK","LRTTTSKPSN" }
+#Question 1
+int = bcell_bepipred.intersection(bcell_iedb)
+print('common epitopes', int) #common sentences
+
+#Question 2
+ideb_unique =  bcell_iedb - bcell_bepipred 
+print("present in ideb" , ideb_unique)
+length_ide = len(ideb_unique)
+
+#Question 3 
+bipepr_unique =  bcell_bepipred- bcell_iedb
+length_bip = len(bipepr_unique)
+print(bipepr_unique , ideb_unique)
+
+#Question 4
+print(length_bip + length_ide)
